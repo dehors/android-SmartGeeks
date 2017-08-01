152237b165423bfb96b32df7f3ba7ff8e39bc588
slam24 <luismartinez242011@gmail.com>
2017-08-01 14:06:18 -0600

    feat: Sound click botton

--- app/src/main/java/com/example/slam24/smartgeeks/Main10Activity.java
+++ app/src/main/java/com/example/slam24/smartgeeks/Main10Activity.java
@@ -1,13 +1,29 @@
 package com.example.slam24.smartgeeks;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main10Activity extends AppCompatActivity {
 
+    MediaPlayer mp;
+    Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main10);
+
+        btnClick = (Button) findViewById(R.id.btnClick);
+
+        mp = MediaPlayer.create(this, R.raw.click);
+
+        btnClick.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mp.start();
+            }
+        });
     }
 }
