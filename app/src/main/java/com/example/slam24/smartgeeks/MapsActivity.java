013c2fdd297e78a478a0478debcf14c7a437c130
slam24 <luismartinez242011@gmail.com>
2017-08-13 06:51:29 -0600

    feat: Custom google map

--- app/src/main/java/com/example/slam24/smartgeeks/MapsActivity.java
+++ app/src/main/java/com/example/slam24/smartgeeks/MapsActivity.java
@@ -1,12 +1,15 @@
 package com.example.slam24.smartgeeks;
 
+import android.Manifest;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -40,7 +43,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").snippet("This is australia").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)).draggable(true));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        int permissionCheck = ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(permissionCheck == 0){
+            mMap.setMyLocationEnabled(true);
+        }
     }
 }
